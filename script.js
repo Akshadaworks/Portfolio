7759716bf3850d59c2e08342743067902c2de23c
Kartik <kartikd22comp@student.mes.ac.in>
2026-04-03 15:50:35 +0530

    0.2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Smooth scroll
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    if (this.getAttribute('href').startsWith("#")) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
 function downloadResume() {
   window.open("resume.pdf", "_blank");
 }
